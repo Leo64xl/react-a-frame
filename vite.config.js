b65f2ce453900e79fdf8f209eec89f08167ca750
Leonardo Portela <portelareinosol@gmail.com>
2026-02-10 23:48:10 -0600

    fix: remove base path for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/react-a-frame' : '/',
+  base: '/',
   plugins: [
     react({
       babel: {
